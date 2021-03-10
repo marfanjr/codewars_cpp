cf469c4249994bc76c4162b768f0cd5727225d01
Marfan Fragoso <marfan@favotecnologia.com.br>
2021-03-10 20:07:29 -0300

    oops

--- string_ends_with/string_ends_with.cpp
+++ string_ends_with/string_ends_with.cpp
@@ -1,8 +1,10 @@
-#include <string>
-#include <iostream>
 bool solution(std::string const &str, std::string const &ending) {
+  if (ending.length() == 0) return true;
+
   std::size_t pos = str.length() - ending.length();
+
   std::size_t found = str.find(ending);
+
   if (found != std::string::npos)
     if(found == pos)
       return true;
